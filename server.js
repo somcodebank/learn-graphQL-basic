fd08488b41e6fb2826466d3ce50e8ede5d0d17dc
somcodebank <som.github@gmail.com>
2020-05-17 17:49:21 -0700

    implement resolvers object

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-const { gql } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
+
+// 1. definition
+// - 1.1 schema (template literal tagged with gql funcyion)
 const typeDefs = gql`
+  schema {
+    query: Query
+  }
+
   type Query {
     greeting: String
   }
 `;
+// - 1.2 resolver OBJECT
+const resolvers = {
+  Query: {
+    greeting: () => "Hello from GraphQL",
+  },
+};
 
-console.log(typeDefs);
+// 2. server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server
+  .listen({ port: 3300 })
+  .then(({ url }) => console.log(`Apollo Server is running at ${url}...`));
